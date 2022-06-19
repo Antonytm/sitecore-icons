b071e1e34c7727ca9106891f5beb7d9e6c96317f
Antonytm <antontishchenko@gmail.com>
2022-06-20 01:37:06 +0300

    Add null check to avoid edge cases

--- scripts/MuiIcon.ps1
+++ scripts/MuiIcon.ps1
@@ -8,13 +8,15 @@ Param(
 $newXml = $SvgContent
 
 $match = Select-String -Pattern "<path[^\/]*\/>|<polygon[^\/]*\/>|<rect[^\/]*\/>" -InputObject $SvgContent -AllMatches
-$match.Matches | ForEach-Object {
-  if ($_.ToString().IndexOf("fill=") -eq -1) {
-    $from = $_
-    $to = $from.ToString().Replace("<path", "<path fill=`"$colorValue`"")
-    $to = $to.ToString().Replace("<polygon", "<polygon fill=`"$colorValue`"")
-    $to = $to.ToString().Replace("<rect", "<rect fill=`"$colorValue`"")
-    $newXml = $newXml.Replace($from, $to)
+if($null -ne $match){
+  $match.Matches | ForEach-Object {
+    if ($_.ToString().IndexOf("fill=") -eq -1) {
+      $from = $_
+      $to = $from.ToString().Replace("<path", "<path fill=`"$colorValue`"")
+      $to = $to.ToString().Replace("<polygon", "<polygon fill=`"$colorValue`"")
+      $to = $to.ToString().Replace("<rect", "<rect fill=`"$colorValue`"")
+      $newXml = $newXml.Replace($from, $to)
+    }
   }
 }
 $newXml = $newXml.Replace("height=`"24`" viewBox=`"0 0 24 24`" width=`"24`"", "height=`"512`" viewBox=`"0 0 24 24`" width=`"512`"")
