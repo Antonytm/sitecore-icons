c6350ec239b15b871ec475beaec001b7dd7775cf
Antonytm <antontishchenko@gmail.com>
2022-06-20 01:33:45 +0300

    Adding comments to code

--- scripts/Main.ps1
+++ scripts/Main.ps1
@@ -7,6 +7,8 @@ if(Test-Path -Path $output) {
 New-Item -Path $output -ItemType Directory
 
 # Process MUI Icons
+# There are 5 types of MUI icons: regular, sharp, outlined, rounded, two tones. To decrease amount of icons to process, we'll only process regular icons.
+# If you want to use other type of icons change -Match parameter
 & .\Generic.ps1 -Prefix "mui-" -Path "../input/mui/material-ui/packages/mui-icons-material/material-icons" -Match "^(?!.*(_sharp_|_outlined_|_rounded_|_two_tone_)).*$"
 # Process FA Icons
 & .\Generic.ps1 -Prefix "fa-" -Path "../input/fa/Font-Awesome/svgs"
